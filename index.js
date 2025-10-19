3048c63a851aeadd406905f6bdf1796890d1b4b7
vkboo <tyounami@163.com>
2025-10-19 23:56:59 +0800

    feat: main logic

---
+++ index.js
@@ -0,0 +1,14 @@
+const LETTER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const DIGIT = '0123456789';
+
+const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const l = () => LETTER[random(0, LETTER.length - 1)];
+const d = () => DIGIT[random(0, DIGIT.length - 1)];
+const codename = () => l() + l() + '-' + d() + d();
+
+const count = process.argv[2] ?? 1;
+for (let i = 0; i < count; i++) {
+    console.log(codename());
+}
+
